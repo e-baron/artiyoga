6488f7ced50d9b200f303f0287a1994edd6b5eef
e-baron <raphael.baroni@vinci.be>
2021-04-10 14:15:57 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteMetadata: {
+    title: "artiYoga",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
